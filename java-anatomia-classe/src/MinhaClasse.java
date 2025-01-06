57b9d7e2ae814e2cf0c07af36b23f8896d5c5109
Diego-Moretti <diegocarmino@hotmail.com>
2025-01-06 10:37:53 -0300

    aula anatomia de classe 2

--- java-anatomia-classe/src/MinhaClasse.java
+++ java-anatomia-classe/src/MinhaClasse.java
@@ -1,7 +1,15 @@
 public class MinhaClasse {
 
 public static void main (String [] args) {
-    System.out.print ("Olá turma, sejam bem vindos");
+    
+    int ano = 2021;
+
+    ano = 2022;
+    // final indica que a variável não pode ser modificada
+    
+    final String BR = "Brasil";
+
+    
 }
 
 }
