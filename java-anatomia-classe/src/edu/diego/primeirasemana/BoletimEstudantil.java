48b0a1babc0dc96fedc167e4d0992a7295410cb8
Diego-Moretti <diegocarmino@hotmail.com>
2025-01-08 17:25:26 -0300

    aula anatomia de classes 5

--- java-anatomia-classe/src/edu/diego/primeirasemana/BoletimEstudantil.java
+++ java-anatomia-classe/src/edu/diego/primeirasemana/BoletimEstudantil.java
@@ -1,3 +1,5 @@
+package edu.diego.primeirasemana;
+
 public class BoletimEstudantil {
     public static void main(String[] args) {
         int mediaFinal = 7;
--- java-anatomia-classe/src/edu/diego/segundasemana/MinhaClasse.java
+++ java-anatomia-classe/src/edu/diego/segundasemana/MinhaClasse.java
@@ -1,3 +1,6 @@
+package edu.diego.segundasemana;
+
+
 public class MinhaClasse {
 
 public static void main (String [] args) {
